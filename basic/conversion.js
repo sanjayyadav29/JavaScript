3f45300d563013eed88e64bab8c446df621a870b
sanjayyadav29 <103998451+sanjayyadav29@users.noreply.github.com>
2023-12-21 07:33:50 +0000

    conversion

---
+++ basic/conversion.js
@@ -0,0 +1,14 @@
+let score = 56;
+
+let name="Sanjay";
+
+console.log(score);
+
+console.log(name);
+
+let score1= "4534";
+
+let numberScore=Number(score1);
+console.log(score);
+
+console.log(numberScore);
