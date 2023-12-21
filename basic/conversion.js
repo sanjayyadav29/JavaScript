882fa45ced5f988775fc50bbe765dc4cf885c2bf
sanjayyadav29 <103998451+sanjayyadav29@users.noreply.github.com>
2023-12-21 08:03:29 +0000

    conparision

--- basic/conversion.js
+++ basic/conversion.js
@@ -1,14 +1,27 @@
+//// Conversion
+
 let score = 56;
 
 let name="Sanjay";
 
-console.log(score);
+// console.log(score);
 
-console.log(name);
+// console.log(name);
 
 let score1= "4534";
 
 let numberScore=Number(score1);
-console.log(score);
+// console.log(score);
 
-console.log(numberScore);
+// console.log(numberScore);
+
+
+/// Operations
+
+let value=34;
+let negVal = -value;
+console.log(negVal);
+
+//
+console.log(null>=0);// because conversion convert null to 0
+console.log(null>0);// because conversion convert null to 0
